6f147d4315ee6f1894624103f0e55d375b35717f
Leandro Kornelius <leobelko@gmail.com>
2023-07-09 17:52:59 -0300

    add campo denuncia para referenciar estudante e removi coluna errada de disciplina em turma

--- Banco-Dados-Mysql/TabelasEntidades/denuncia.sql
+++ Banco-Dados-Mysql/TabelasEntidades/denuncia.sql
@@ -4,5 +4,6 @@ DROP TABLE IF EXISTS `denuncia`;
 
 CREATE TABLE `denuncia` (
     `Id_Denuncia` int PRIMARY KEY AUTO_INCREMENT,
+    FOREIGN KEY (fk_Estudante) REFERENCES estudante(Matricula),
     FOREIGN KEY (fk_Avaliacao) REFERENCES avaliacao(Id_Avaliacao),
 )
--- Banco-Dados-Mysql/TabelasEntidades/disciplina.sql
+++ Banco-Dados-Mysql/TabelasEntidades/disciplina.sql
@@ -6,6 +6,5 @@ CREATE TABLE `disciplina` (
     `Codigo_Disciplina` VARCHAR(10) PRIMARY KEY,
     `Nome_Disciplina` VARCHAR(100),
     FOREIGN KEY (fk_Professor) REFERENCES professor(Id_Professor),
-    FOREIGN KEY (fk_Turma) REFERENCES turma(Id_Turma),
     FOREIGN KEY (fk_Departamento) REFERENCES departamento(Codigo_Departamento),
 )
